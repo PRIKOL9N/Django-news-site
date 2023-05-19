919338886336329c1538cdf9533ca4fdcfbdb75d
Prikol9 <nikset1@gmail.com>
2023-05-19 12:43:30 +0300

    release v1.3

--- mysite/news/templatetags/news_tags.py
+++ mysite/news/templatetags/news_tags.py
@@ -5,7 +5,6 @@ from django.core.cache import cache
 from news.models import Category
 
 
-
 register = template.Library()
 
 
@@ -16,11 +15,5 @@ def get_categories():
 
 @register.inclusion_tag('news/list_categories.html')
 def show_categories(arg1='Hello', arg2='world' ):
-    # categories = Category.objects.all()
-    # categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
-    # categories = cache.get('categories')
-    # if not categories:
-    #     categories = Category.objects.annotate(cnt=Count('news', filter=F('news__is_published'))).filter(cnt__gt=0)
-    #     cache.set('categories', categories, 30)
     categories = Category.objects.annotate(cnt=Count('news', filter=F('news__is_published'))).filter(cnt__gt=0)
     return {'categories': categories, 'arg1': arg1, 'arg2': arg2}
--- mysite/news/utils.py
+++ mysite/news/utils.py
@@ -1,5 +1,5 @@
 class MyMixin(object):
-    mixin_prop =  ''
+    mixin_prop = ''
 
     def get_prop(self):
         return self.mixin_prop.upper()
